d90dcf3da07d515abe83e8c5c4c611d20876c17b
Nguyen Nhat <nhat.nguyen@kingfood.co>
2021-03-05 01:53:44 +0700

    add api

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Put, Delete, Res, Req, HttpStatus, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, Req, Param, HttpStatus, Body, Query } from '@nestjs/common';
 import { Response, Request } from 'express';
-import { CreateProductsDto } from './dto/products.dto';
+import { CreateProductsDto, FilterProductsDto } from './dto/products.dto';
 import { ProductsService } from './products.service';
 @Controller('products')
 export class ProductsController {
@@ -9,6 +9,19 @@ export class ProductsController {
     @Post()
     async createPost(@Req() req, @Res() res, @Body() createProductsDto: CreateProductsDto){
         const product = await this.productsService.createProducts(createProductsDto)
+        res.status(HttpStatus.OK).send(product)
+    }
+
+    @Get()
+    async getPosts(@Res() res, @Query() query: FilterProductsDto) {
+        console.log(query);
+        const products = await this.productsService.getProducts(query)
+        res.status(HttpStatus.OK).send(products)
+    }
+
+    @Get('/:productId')
+    async getPost(@Res() res, @Param('productId') productId: string) {
+        const product = await this.productsService.getProduct(productId)
         res.status(HttpStatus.OK).send(product)
     }
 }
